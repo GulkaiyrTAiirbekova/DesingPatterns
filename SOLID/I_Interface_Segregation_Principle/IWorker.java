af4483ccf987539f79a3a8d872653bc03ee5add1
Your Name <taiirbekovag@gmail.com>
2025-11-23 21:58:04 -0930

    Builder Pattern example of BuilderDemo

---
+++ SOLID/I_Interface_Segregation_Principle/IWorker.java
@@ -0,0 +1,14 @@
+//package SOLID.I_Interface_Segregation_Principle;
+//
+//public interface IWorker {
+//    void work();
+//    void eat();
+//    void sleep();
+//}
+//class Human implements IWorker{...} //Implements all 3
+//
+//class Robot implements IWorker{
+//    void work(){...}
+//    void eat(){/* do nothing*/} //Forced to implemets
+//    void sleep(){/*do nothing*/} //Forced to implement
+//}
