12ba4f41c1eed06c43508536f7209ecd54495457
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-08 03:25:21 +0600

    git commit -m "Implement HighState with transition from HIGH to OFF and undo to MEDIUM"

---
+++ Behavioral/State/HighState.java
@@ -0,0 +1,15 @@
+package Behavioral.State;
+
+class HighState implements FanState {
+    @Override
+    public void handleRequest(Fan fan) {
+        System.out.println("Fan turned off");
+        fan.setState(new OffState());
+    }
+
+    @Override
+    public void undoRequest(Fan fan) {
+        System.out.println("Fan turned on medium.");
+        fan.setState(new MediumState());
+    }
+}
