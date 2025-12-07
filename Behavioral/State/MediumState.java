2a709c1f99a7d5d1b04b2e7263528d1f1c349965
Gulkaiyr Taiirbekova <taiirbekovag@gmail.com>
2025-12-08 03:24:32 +0600

    git commit -m "Implement MediumState enabling transition to HIGH and undo back to LOW"

---
+++ Behavioral/State/MediumState.java
@@ -0,0 +1,14 @@
+package Behavioral.State;
+
+class MediumState implements FanState {
+    @Override
+    public void handleRequest(Fan fan) {
+        System.out.println("Fan turned on high.");
+        fan.setState(new HighState());
+    }
+    @Override
+    public void undoRequest(Fan fan){
+        System.out.println("Fan turned on low");
+        fan.setState(new LowState());
+    }
+}
